7a3147a527fa7106c207e7928785055d9a5efc78
Andrew Erickson <aerickson@gmail.com>
2019-01-10 18:44:46 -0800

    Update clamp.rb
    switch to using my master

--- clamp.rb
+++ clamp.rb
@@ -1,7 +1,7 @@
 class Clamp < Formula
   desc "command-line tool for local MAMP management"
   homepage "https://jide.github.io/clamp"
-  url "https://github.com/jide/clamp.git", :using => :git, :tag => "1.4.2", :revision => "6ab13777fa4f3702d63a7fd77041c929a0045388"
+  url "https://github.com/aerickson/clamp.git", :using => :git
 
   depends_on "mariadb"
 
